14fa966c79db176269fc343685ea6da2dded0aa6
Nicolas Silva <155106116+nicolassilv04@users.noreply.github.com>
2025-09-26 11:03:32 -0300

    Delete webScraping_BeatifulSoap.py

--- webScraping_BeatifulSoap.py
+++
@@ -1,8 +0,0 @@
-import requests, bs4
-res = requests.get('lista.mercadolivre.com.br/playstation')
-res.raise_for_status()
-noStarchSoup = bs4.BeautifulSoup(res.text)
-for link in noStarchSoup.select('a'):
-    print(str(link))
-tag = noStarchSoup.select('a')[0]
-tag.attrs
