e2f5cf4cb79957d356db67ab9d5b408ace9f9c25
Nicolas Silva <155106116+nicolassilv04@users.noreply.github.com>
2025-09-26 11:02:56 -0300

    Delete miniProjeto_RaspagemDados.py

--- miniProjeto_RaspagemDados.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-from bs4 import BeautifulSoup
-
-#FAZENDO A PESQUISA DO ITEM
-pesquisa = str(input('O que deseja pesquisar: '))
-webbrowser.open('lista.mercadolivre.com.br/' + pesquisa)
-
-
