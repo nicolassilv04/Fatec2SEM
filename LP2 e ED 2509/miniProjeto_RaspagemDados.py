3cd8b209def696d6217fcaa4819e72cc3d840467
Nicolas Silva <nicolasfsilva2004@gmail.com>
2025-10-06 10:09:50 -0300

    atualização codigo

--- LP2 e ED 2509/miniProjeto_RaspagemDados.py
+++ LP2 e ED 2509/miniProjeto_RaspagemDados.py
@@ -7,56 +7,55 @@ COMO FUNCIONA:
 E SÃO RETORNADOS OS RESULTADOS BASEADOS NO CODIGO HTML DO SITE
 OS ITENS RETORNADOS SÃO ANALISADOS, ENTRAM PARA UMA LISTA DE DICIONARIO E PODEM SER PRINTADOS
 
-POR CONTA DO PRINT SER CONFUSO NESSE CASO, OS ITENS SÃO CONVERTIDOS PARA CSV E POSTERIOMENTE VISUALIZADOS PELO PANDAS
 '''
 
 import requests
 from bs4 import BeautifulSoup
 import csv
-import pandas as pd
 
 lista_produtos = []
 
-produto_pesquisa = input('Insira qual produto deseja pesquisar: ')
-if ' ' in produto_pesquisa:
-    produto_pesquisa = produto_pesquisa.replace(' ','-')
+def converter_preco(preco_str):
+    # Remove R$, espaços e substitui vírgula por ponto
+    preco_str = preco_str.replace("R$", "").replace(".", "").replace(",", ".").strip()
+    return float(preco_str)
+
+def pesquisa_mercado_livre(produto):
+    if ' ' in produto:
+        produto = produto.replace(' ','-')
 
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-}
-url = 'https://lista.mercadolivre.com.br/'
-pagina = requests.get(url + produto_pesquisa, headers=headers)
-
-dados_pagina = BeautifulSoup(pagina.text, 'html.parser')
-
-procura_titulo = dados_pagina.find_all('h3', class_='poly-component__title-wrapper')
-procura_precos = dados_pagina.find_all('span', class_ = 'andes-money-amount__fraction')
-procura_link = dados_pagina.find_all('a', class_='poly-component__title')
-
-for titulo, preco, link in zip (procura_titulo, procura_precos, procura_link):
-    produto_dicionario = {
-        'DESCRIÇÃO': titulo.text,
-        'PREÇO': preco.text,
-        'LINK': link['href']
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-    lista_produtos.append(produto_dicionario)
+    url = 'https://lista.mercadolivre.com.br/'
+    pagina = requests.get(url + produto, headers=headers)
 
-lista_ordenada = sorted(lista_produtos, key=lambda produto: int(produto['PREÇO'].replace('.', '')))
+    dados_pagina = BeautifulSoup(pagina.text, 'html.parser')
 
-nome_arquivo = 'produtos_mercado_livre.csv'
-with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo_csv:
-    colunas = ['DESCRIÇÃO', 'PREÇO', 'LINK']
-    escritor = csv.DictWriter(arquivo_csv, fieldnames=colunas)
+    procura_titulo = dados_pagina.find_all('h3', class_='poly-component__title-wrapper')
+    procura_precos = dados_pagina.find_all('span', class_ = 'andes-money-amount__fraction')
+    procura_link = dados_pagina.find_all('a', class_='poly-component__title')
 
-    escritor.writeheader()
-    escritor.writerows(lista_ordenada)
+    for titulo, preco, link in zip (procura_titulo, procura_precos, procura_link):
+        produto_dicionario = {
+            'DESCRIÇÃO': titulo.text,
+            'PREÇO': preco.text,
+            'LINK': link['href']
+        }
+        lista_produtos.append(produto_dicionario)
 
-print(f"\nOs dados foram salvos com sucesso no arquivo '{nome_arquivo}'!")
+    nome_arquivo = 'produtos_mercado_livre_novo.csv'
+    with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo_csv:
+        colunas = ['DESCRIÇÃO', 'PREÇO', 'LINK']
+        escritor = csv.DictWriter(arquivo_csv, fieldnames=colunas)
 
+        escritor.writeheader()
+        escritor.writerows(lista_produtos)
 
-df = pd.read_csv(nome_arquivo)
-print(df)
+    print(f"\nOs dados foram salvos com sucesso no arquivo '{nome_arquivo}'!")
+    return nome_arquivo
 
+pesquisa_mercado_livre('xbox series s')#insira seu produto aqui
 
 
